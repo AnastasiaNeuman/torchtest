80848df0315eabe7d0722bd429d1b5182352b638
aneuman1114 <82843858+aneuman1114@users.noreply.github.com>
2025-03-11 21:08:59 -0400

    init nn

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import torch
+print(torch.__version__)
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
@@ -10,7 +11,7 @@ from torchvision.transforms import ToTensor
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
-    download=True,
+    download=False,
     transform=ToTensor(),
 )
 
@@ -18,7 +19,7 @@ training_data = datasets.FashionMNIST(
 test_data = datasets.FashionMNIST(
     root="data",
     train=False,
-    download=True,
+    download=False,
     transform=ToTensor(),
 )
 
@@ -37,3 +38,31 @@ for X, y in test_dataloader:
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
 
+#to create a neural net, we define a class that inherits from nn.Module 
+#we define the layers in the __init__ function
+#we specify how data will pass through the network in the forward function
+
+#this checks if an accelerator is available to accelerate NN operations
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
